425a17b31c381ff83dd8c9c2e19614ecfbd0ebe7
adityabhamare10 <adityabhamare71@gmail.com>
2024-10-15 15:12:16 +0530

    onCreate()

---
+++ app/src/main/java/com/aditya/lifecycleapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.aditya.lifecycleapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Toast.makeText(this,
+            "onCreate() is called",
+            Toast.LENGTH_SHORT
+        ).show()
+
+    }
+
+
+}
