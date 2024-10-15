a3a5d2d97ccc9ccffff8dc124a0c1e557d854b4f
adityabhamare10 <adityabhamare71@gmail.com>
2024-10-15 15:47:05 +0530

    i.putExtra() (message Passing)

--- app/src/main/java/com/aditya/lifecycleapp/MainActivity.kt
+++ app/src/main/java/com/aditya/lifecycleapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aditya.lifecycleapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     lateinit var btn : Button
+    lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +27,13 @@ class MainActivity : AppCompatActivity() {
 
 
         btn = findViewById(R.id.goBtn)
+        textView = findViewById(R.id.textView)
         btn.setOnClickListener {
             //Explicit Intent
             val i = Intent(this, MainActivity2::class.java)
+            i.putExtra("msg", textView.text.toString())
             startActivity(i)
+
         }
 
 
--- app/src/main/java/com/aditya/lifecycleapp/MainActivity2.kt
+++ app/src/main/java/com/aditya/lifecycleapp/MainActivity2.kt
@@ -1,12 +1,16 @@
 package com.aditya.lifecycleapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        textView = findViewById(R.id.textView)
+
+        var msg = intent.getStringExtra("msg")
+        textView.text = "$msg"
+
+
     }
 }
