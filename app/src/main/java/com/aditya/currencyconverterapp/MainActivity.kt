f29633f64b01bb353b2b53572512cf757332db2d
adityabhamare10 <adityabhamare71@gmail.com>
2024-10-15 14:44:03 +0530

    First Commit

---
+++ app/src/main/java/com/aditya/currencyconverterapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.aditya.currencyconverterapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+//    lateinit (late initiate) used when we use it later so it initiate later when we actually use it to avoid null pointer exception
+    lateinit var editTextusd : EditText
+    lateinit var btn : Button
+    lateinit var result : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editTextusd = findViewById(R.id.edt_usd)
+        btn = findViewById(R.id.button)
+        result = findViewById(R.id.result)
+
+        btn.setOnClickListener{
+
+            var usd: String = editTextusd.text.toString()
+
+            var usdDoubled: Double = usd.toDouble()
+
+            result.setText("₹"+convert(usdDoubled).toString())
+
+
+        }
+
+    }
+
+    fun convert(usd:Double): Double{
+        return usd * 84.06
+    }
+
+}
