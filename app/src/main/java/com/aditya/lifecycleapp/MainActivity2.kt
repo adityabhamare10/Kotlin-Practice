eb4ce6ef0235f0591124e5ba9c5f9782c8645c0f
adityabhamare10 <adityabhamare71@gmail.com>
2024-10-15 15:36:16 +0530

    Explicit Intent

--- app/src/main/java/com/aditya/lifecycleapp/MainActivity2.kt
+++ app/src/main/java/com/aditya/lifecycleapp/MainActivity2.kt
@@ -1,39 +1,20 @@
 package com.aditya.lifecycleapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var btn : Button
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        btn = findViewById(R.id.goBtn)
-        btn.setOnClickListener {
-            //Explicit Intent
-            val i = Intent(this, MainActivity2::class.java)
-            startActivity(i)
-        }
-
-
     }
-
-
-
 }
